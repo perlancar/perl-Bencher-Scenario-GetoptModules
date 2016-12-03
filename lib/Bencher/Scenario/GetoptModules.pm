b5e2183454f8f965195518ed227b3b4d8a93d3d4
perlancar (@pc-home) <perlancar@gmail.com>
2016-12-03 14:57:34 +0700

    Add participants: Getopt::ArgParse, Getopt::Long::More, Getopt::Compact, Getopt::Alt

--- lib/Bencher/Scenario/GetoptModules.pm
+++ lib/Bencher/Scenario/GetoptModules.pm
@@ -13,21 +13,40 @@ our $scenario = {
     module_startup => 1,
 
     participants => [
+
+        {module=>'Getopt::Std'},
+
+        # Getopt::Long-compatible interface or wrapper
         {module=>'Getopt::Long', tags=>['getopt-long']},
         {module=>'Getopt::Long::Less', tags=>['getopt-long']},
         {module=>'Getopt::Long::EvenLess', tags=>['getopt-long']},
         {module=>'Getopt::Long::Complete', tags=>['getopt-long']},
         {module=>'Getopt::Long::Descriptive', tags=>['getopt-long']},
         {module=>'Getopt::Long::Subcommand', tags=>['getopt-long']},
+        {module=>'Getopt::Long::More', tags=>['getopt-long']},
+        {module=>'Getopt::Compact', tags=>['getopt-long']},
 
-        {module=>'Getopt::Std'},
+        # can't be loaded independently
+        # {module=>'MooX::Options', tags=>['moo']},
 
-        {module=>'MooseX::Getopt', tags=>['moox']},
+        {module=>'MooseX::Getopt', tags=>['moose']},
 
         {module=>'Getopt::ArgvFile'},
+
         {module=>'Getopt::Lucid'},
+
         {module=>'Getopt::Panjang'},
+
         {module=>'Getopt::Std::Strict'},
+
+        {module=>'Getopt::Alt'},
+
+        # nodejs-inspired
+        {module=>'Smart::Options'},
+
+        # python-inspired
+        {module=>'Getopt::ArgParse'},
+
     ],
 };
 
